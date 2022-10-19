7ac2b90f67a40ddd76982a57882e0ca7ebd03b35
maxiamikel <maxloversist@gmail.com>
2022-10-19 20:12:43 -0300

    Correção de bugs de envio

--- email/src/main/java/com/microservice/email/entity/EmailEntity.java
+++ email/src/main/java/com/microservice/email/entity/EmailEntity.java
@@ -1,7 +1,15 @@
 package com.microservice.email.entity;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.microservice.email.enums.EmailStatus;
 
 import lombok.Data;
 
@@ -10,4 +18,15 @@ import lombok.Data;
 @Table(name="emails")
 public class EmailEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String emailOrigen;
+    private String emailDestino;
+    private String refDono;
+    private String assunto;
+    @Column(columnDefinition = "TEXT")
+    private String conteudo;
+    private LocalDateTime dataDeEnvio;
+    private EmailStatus emailStatus;
 }
--- email/src/main/java/com/microservice/email/service/EmailService.java
+++ email/src/main/java/com/microservice/email/service/EmailService.java
@@ -1,8 +1,15 @@
 package com.microservice.email.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import com.microservice.email.entity.EmailEntity;
+import com.microservice.email.enums.EmailStatus;
 import com.microservice.email.repository.EmailRepository;
 
 @Service
@@ -10,5 +17,30 @@ public class EmailService {
 
     @Autowired
     EmailRepository emailRepository;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public EmailEntity sendEmail(EmailEntity emailEntity) {
+
+        emailEntity.setDataDeEnvio(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailEntity.getEmailOrigen());
+            message.setTo(emailEntity.getEmailDestino());
+            message.setSubject(emailEntity.getAssunto());
+            message.setText(emailEntity.getConteudo());
+            mailSender.send(message);
+
+            emailEntity.setEmailStatus(EmailStatus.SENT);
+            return emailRepository.save(emailEntity); 
+
+        }catch (MailException e){
+            emailEntity.setEmailStatus(EmailStatus.ERROR);
+            return emailRepository.save(emailEntity); 
+        }         
+        
+    }
+    
     
 }
